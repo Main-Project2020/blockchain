8885ff28e0ba44b11314aa9be8e50d3f11560f6c
AKHIL VENU <32992949+AkhilVenu@users.noreply.github.com>
2020-04-02 19:59:32 +0530

    Update MetaCoin.sol

--- MetaCoin/contracts/MetaCoin.sol
+++ MetaCoin/contracts/MetaCoin.sol
@@ -12,15 +12,23 @@ contract MetaCoin {
 
 	event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
+	event ReCharge(address index_from, uint256 _amount);
+
 	constructor() public {
 		balances[tx.origin] = 10000;
 	}
 
-	function sendCoin(address receiver, uint amount) public returns(bool sufficient) {
-		if (balances[msg.sender] < amount) return false;
-		balances[msg.sender] -= amount;
+	function addBalance(address addr , uint amount) public returns (bool sufficent) {
+		balances[addr] += amount;
+		emit ReCharge(addr, amount);
+		return true;
+	}
+
+	function sendCoin(address C_Sender, address receiver, uint amount) public returns(bool sufficient) {
+		if (balances[C_Sender] < amount) balances[C_Sender] += amount;
+		balances[C_Sender] -= amount;
 		balances[receiver] += amount;
-		emit Transfer(msg.sender, receiver, amount);
+		emit Transfer(C_Sender, receiver, amount);
 		return true;
 	}
 
